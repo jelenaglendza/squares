0bf4689e0423cdf13ee3107e1b1edf5b1aaf3d6b
jelenaglendza <glendza99@hotmail.com>
2021-03-04 12:52:49 +0100

    #2 - Implemented interactive square component (#2)
    * Deleted "Hello World" component
    
    * Made the background yellow
    
    * Created a square component and placed it three times
    
    * Binded a click event to the square components
    
    * Printed the number of the clicked square in the middle square
    
    * Colored the clicked square red

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <main>
+      <!-- eslint-disable-next-line max-len -->
+      <square
+        :ref="`square`+i"
+        v-for="i in this.numbers"
+        :key="i" :number="i"
+        @onclick="onClick"
+      ></square>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Square from './components/Square.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Square,
+  },
+  data() {
+    return {
+      numbers: [1, 2, 3],
+    };
+  },
+  methods: {
+    onClick(i) {
+      this.printNumber(i);
+      this.colorClickedSquare(i);
+    },
+    printNumber(i) {
+      this.$refs.square2.setDisplayText(i);
+    },
+    colorClickedSquare(i) {
+      this.numbers
+          .map((n) => this.$refs['square' + n])
+          .forEach((square) => square.colorSquareIfClicked(i));
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  body, html {
+    height: 100vh;
+    margin: 0px;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+    background-color: yellow;
+    display: flex;
+    align-items: center;
+  }
+
+  main {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+
+  }
 </style>
---
+++ src/components/Square.vue
@@ -0,0 +1,49 @@
+<template>
+    <!-- eslint-disable-next-line max-len -->
+    <div ref="square" class="square" v-on:click="emitNumber">
+      <p ref="displayText">{{ displayText }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Square',
+  props: ['number'],
+  data() {
+    return {
+      displayText: '',
+    };
+  },
+  methods: {
+    emitNumber() {
+      this.$emit('onclick', this.number);
+    },
+    setDisplayText(text) {
+      this.displayText = text;
+    },
+    colorSquareIfClicked(i) {
+      if (i === this.number) {
+        this.$refs.square.style.backgroundColor = 'red';
+      } else {
+        this.$refs.square.style.backgroundColor = 'blue';
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .square {
+      width: 300px;
+      height: 300px;
+      background-color: blue;
+  }
+
+  p {
+    font-size: 6em;
+    display: flex;
+    color: white;
+    align-items: center;
+    justify-content: space-around;
+  }
+</style>
